d528b52bc22f1911eb9e95739ef0aef03256328e
Kobayashi Akihiro <kobayashi.akihiro@leadinge.co.jp>
2012-02-19 14:39:49 +0900

    add gameactivity

---
+++ src/com/pigmal/android/accessory/GameActivity.java
@@ -0,0 +1,63 @@
+package com.pigmal.android.accessory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.pigmal.android.accessory.view.MySurfaceView;
+import com.pigmal.android.ex.accessory.R;
+
+public class GameActivity extends Activity  implements OnClickListener{
+    MySurfaceView surfaceView;
+    Button buttonUp;
+    Button buttonDown;
+    Button buttonLeft;
+    Button buttonRight;
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_main);
+        
+        surfaceView = (MySurfaceView)findViewById(R.id.mySurfaceView1);
+        
+        buttonUp = (Button)findViewById(R.id.button_up);
+        buttonUp.setOnClickListener(this);
+        buttonDown = (Button)findViewById(R.id.button_down);
+        buttonDown.setOnClickListener(this);
+        buttonLeft = (Button)findViewById(R.id.button_left);
+        buttonLeft.setOnClickListener(this);
+        buttonRight = (Button)findViewById(R.id.button_right);
+        buttonRight.setOnClickListener(this);
+        
+
+    }
+    
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.button_left:
+            surfaceView.move(KeyEvent.KEYCODE_DPAD_LEFT, 10);
+            
+            break;
+        case R.id.button_right:
+            surfaceView.move(KeyEvent.KEYCODE_DPAD_RIGHT, 10);
+            
+            break;
+        case R.id.button_up:
+            surfaceView.move(KeyEvent.KEYCODE_DPAD_UP, 10);
+            break;
+        case R.id.button_down:
+            surfaceView.move(KeyEvent.KEYCODE_DPAD_DOWN, 10);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+}
---
+++ src/com/pigmal/android/accessory/view/MySurfaceView.java
@@ -0,0 +1,97 @@
+package com.pigmal.android.accessory.view;
+
+import com.pigmal.android.ex.accessory.R;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private Bitmap img;
+    private boolean isRunning = true;
+    private Thread thread;
+    private Paint paint;
+    int x, y;
+    int vX, vY;
+
+    public MySurfaceView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MySurfaceView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        getHolder().addCallback(this);
+        paint = new Paint();
+        img = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_launcher_demokit);
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            draw();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        thread = null;
+    }
+    
+    public void move(int direction, int accell){
+        switch (direction) {
+        case KeyEvent.KEYCODE_DPAD_DOWN:
+            vY = vY + accell;
+            break;
+        case KeyEvent.KEYCODE_DPAD_UP:
+            vY = vY - accell;;
+            break;
+        case KeyEvent.KEYCODE_DPAD_LEFT:
+            vX = vX - accell;
+            break;
+        case KeyEvent.KEYCODE_DPAD_RIGHT:
+            vX = vX + accell;
+            break;
+
+        default:
+            break;
+        }
+        
+    }
+    
+    private void draw(){
+        Canvas canvas = getHolder().lockCanvas();
+        canvas.drawColor(Color.GRAY);
+        canvas.drawBitmap(img, vX, vY, paint);
+        getHolder().unlockCanvasAndPost(canvas);
+
+    }
+    
+}
